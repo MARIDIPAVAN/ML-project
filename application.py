da4ad9b78e38c89d6013b37aed234a0b8c6e896d
pavan maridi <pavanmaridi1205@gmail.com>
2024-06-05 18:48:58 +0530

    extensions

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def predict_datapoint():
             return render_template('home.html', prediction="An error occurred")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
